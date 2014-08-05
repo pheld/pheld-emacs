3945bbac06236a38e434431ec4278505334db65e
pheld <peter.t.held@gmail.com>
2014-08-05 00:52:36 -0700

    Disable Projectile caching

--- custom/projectile-setup.el
+++ custom/projectile-setup.el
@@ -1,6 +1,6 @@
 (require 'helm)
  (projectile-global-mode)
- (setq projectile-enable-caching t)
+;; (setq projectile-enable-caching t)
  (setq projectile-completion-system 'helm)
  ;; Press Command-p for fuzzy find in project
   (global-set-key (kbd "M-p") 'projectile-find-file)
